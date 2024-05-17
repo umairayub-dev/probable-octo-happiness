87ef7b263f60aba5b1105e9ef6cab2747d1dfaad
umairayub-dev <79558757+umairayub-dev@users.noreply.github.com>
2024-05-17 12:06:30 +0500

    Update scrap-controller.js

--- api/controller/scrap-controller.js
+++ api/controller/scrap-controller.js
@@ -9,31 +9,20 @@ module.exports = class ScrapController {
 
         while (count < startPage + maxPages) {
             const url = `https://www.google.com/search?q=${abtData}&start=${count}`;
-            console.log(`Scraping page: ${url}`); // Log current page being scraped
+            const response = await axios(url, {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12'
+                }
+            });
 
-            try {
-                const response = await axios(url, {
-                    headers: {
-                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12'
-                    }
-                });
+            const html = response.data;
+            let $ = cheerio.load(html);
+            let searchRes = $(`a`);
+            let final = html2json(searchRes.toString()).child;
 
-                const html = response.data;
-                let $ = cheerio.load(html);
-                let searchRes = $(`a`);
-                let final = html2json(searchRes.toString()).child;
-
-                results = results.concat(filterInstagramUrls(final));
-            } catch (error) {
-                console.error(`Error scraping page ${url}:`, error.message);
-            }
+            results = results.concat(filterInstagramUrls(final));
 
             count++;
-
-            // Random delay between requests
-            const delay = Math.floor(Math.random() * 2000) + 1000;
-            console.log(`Delaying for ${delay} ms`);
-            await new Promise(resolve => setTimeout(resolve, delay));
         }
 
         return { totalProfiles: results.length, profiles: results };
@@ -42,7 +31,13 @@ module.exports = class ScrapController {
 
 const filterInstagramUrls = (data) => {
     return data
-        .filter(item => item.attr && item.attr.href && item.attr.href.includes('instagram.com') && item.attr.href.includes('url=') && !item.attr.href.includes('google.com'))
+        .filter(item => 
+            item.attr && 
+            item.attr.href && 
+            item.attr.href.includes('instagram.com') && 
+            item.attr.href.includes('url=') && 
+            !item.attr.href.includes('google.com') &&
+            !item.attr.href.includes('/search?q='))
         .map(item => {
             const urlMatch = item.attr.href.match(/url=([^&]*)/);
             return urlMatch ? decodeURIComponent(urlMatch[1]) : null;
